91c1941a6b94be4e48e088bf851930d11a695aed
Matthew Schröder <matthewschroeder94@gmail.com>
2022-09-17 12:12:26 +0200

    add styling to player and board

--- js/main.js
+++ js/main.js
@@ -15,18 +15,30 @@ class Game {
 
 class Player {
     constructor (){
-        this.width = 15;
+        this.width = 10;
         this.height = 10;
         this.positionX = 50;
-        this.positionY = 10;
+        this.positionY = 5;
         this.domElement = null;
 
         this.createDomElement();
     }
     createDomElement(){
-        
+        this.domElement = document.createElement('div');
+        this.domElement.id = 'player';
+        this.domElement.style.width = this.width + 'vw';
+        this.domElement.style.height = this.height + 'vh';
+        this.domElement.style.bottom = this.positionY + 'vh';
+        this.domElement.style.left = this.positionX + 'vw';
+
+        const boardElm = document.getElementById("board");
+        boardElm.appendChild(this.domElement)
     }
     moveLeft(){
 
     }
 }
+
+
+const game = new Game();
+game.start();
